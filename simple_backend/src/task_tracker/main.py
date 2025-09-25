8fa6f4e7a3736fea54c9ece5d65d629a9f3a4274
Ilnur <ilnur.altezza@yandex.ru>
2025-09-25 15:38:55 +0300

    Homework task 2 Completed

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,60 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
+from enum import Enum
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/tasks")
-def get_tasks():
-    pass
+class TaskStatus(str, Enum):
+    todo = "todo"
+    in_progress = "in_progress"
+    done = "done"
 
-@app.post("/tasks")
-def create_task(task):
-    pass
+class Task(BaseModel):
+    id: int
+    title: str
+    status: TaskStatus = TaskStatus.todo
 
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+class TaskCreate(BaseModel):
+    title: str
+    status: TaskStatus = TaskStatus.todo
 
-@app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-    pass
+class TaskUpdate(BaseModel):
+    title: str | None
+    status: TaskStatus | None
+
+tasks: dict[int, Task] = {}
+
+_next_id = 1
+
+@app.get("/tasks", response_model=list[Task])
+def get_tasks() -> list[Task]:
+    return list(tasks.values())
+
+@app.post("/tasks", response_model=Task, status_code=status.HTTP_201_CREATED)
+def create_task(payload: TaskCreate) -> Task:
+    global _next_id
+    new_task = Task(id=_next_id, **payload.model_dump())
+    tasks[_next_id] = new_task
+    _next_id += 1
+    return new_task
+
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, payload: TaskUpdate):
+    if task_id not in tasks:
+        raise HTTPException(status_code=404, detail="Задача не найдена!")
+
+    stored_task = tasks[task_id]
+
+    update_data = payload.model_dump(exclude_unset=True)
+    updated_task = stored_task.model_copy(update=update_data)
+
+    tasks[task_id] = updated_task
+    return updated_task
+
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: int) -> None:
+    if task_id not in tasks:
+        raise HTTPException(status_code=404, detail="Задача не найдена!")
+    tasks.pop(task_id)
+    return None
+
